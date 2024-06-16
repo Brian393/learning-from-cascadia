a88fac2519659213eae963222542260628f76181
Brian393 <bhcontinentaldrift@gmail.com>
2024-06-16 16:53:34 -0500

    Add publicPath for GitHub Pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = defineConfig({
+  publicPath: '/learning-from-cascadia/',
   transpileDependencies: true,
   lintOnSave: false,
   configureWebpack: {
